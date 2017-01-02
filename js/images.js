c0e0a80fe86f3902e056b37806363b606be96cf2
Davis George <dgeorge668@gmail.com>
2017-01-02 13:26:35 -0500

    update root

---
+++ js/images.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	$('#pacman').hover(function() {
+		$('.med').text("Pacman: Written in Java. Ghosts use a Breadth First Search algorithm to chase Pacman.");
+	});
+	$('#yelp').hover(function() {
+		$('.med').text("Database Client: Written in Java. Can execute protected SQL queries for user to visualize.");
+	});
+	$('#eliza').hover(function() {
+		$('.med').text("Eliza: Written in OCaml. Uses recursion to extract regular expressions and respond.");
+	});
+	$('#projects').hover(function() {
+		$('.med').text("Hover over the images for more details.");
+	});
+	$('p').hover(function() {
+		$('.med').text("Hover over the images for more details.");
+	});
+});
+
+
