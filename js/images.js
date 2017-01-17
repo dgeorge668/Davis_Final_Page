7151a2feffd9db59d6baebd90b26069123267f14
Davis George <dgeorge668@users.noreply.github.com>
2017-01-17 18:51:15 -0500

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
 	$('#huffman').hover(function() {
-		$('.med').text("Huffman: Written in Java. Utilizes a greedy algorithm to form a binary-search-tree used in file compression. Click for a full explanation of a Huffman Compression works.");
+		$('.med').text("Huffman: Written in Java. Utilizes a greedy algorithm to form a binary-search-tree used in file compression. Click for a full explanation of how a Huffman Compression works.");
 	});
 	$('#creditz').hover(function() {
-		$('.med').text("Credit Analysis: Written in R. Modeled risk of loan default using Bayesian Model Averaging on dataset of >100,000 loan samples. Click to view the full report. www.google.com");
+		$('.med').text("Credit Analysis: Written in R. Modeled risk of loan default using Bayesian Model Averaging on dataset of >100,000 loan samples. Click to view the full report.");
 	});
 	$('#feedme').hover(function() {
 		$('.med').text("FeedMe: A small business I started and sold which facilitated in-school food deliveries with an online form. Click to see the official website and form I created. ");
